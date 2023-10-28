1bdc9c7618cdf4719cdb487d230abffeaceff6c9
Valeria <valeria.henao@hotmail.com>
2023-10-27 21:44:02 -0500

    Revert "Merge branch 'develop' into qa"
    This reverts commit 4d75fdd1e5fc3cb569a96c27276a6f6e89d91409, reversing
    changes made to b9b4188ba97f75a12799c6e33797be2716195a8d.

--- Front/maqua_frontend/src/components/transactionhistory/TransactionTable.jsx
+++ Front/maqua_frontend/src/components/transactionhistory/TransactionTable.jsx
@@ -31,9 +31,6 @@ const formatCurrency = (amount) => {
 const TransactionTable = (props) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedRows, setSelectedRows] = useState([]);
-  const [selectedStatus, setSelectedStatus] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
-  const transactionsPerPage = 8;
 
   const toggleRowSelection = (rowId) => {
     if (selectedRows.includes(rowId)) {
@@ -50,15 +47,13 @@ const TransactionTable = (props) => {
   const filteredTransactions = props.transactions.filter((transaction) => {
     const searchTermLower = searchTerm.toLowerCase();
     return (
-      (selectedStatus === null || transaction.status === selectedStatus) &&
-      (transaction.id.toString().includes(searchTermLower) ||
-        statusMap[transaction.status].toLowerCase().includes(searchTermLower) ||
-        formatCurrency(transaction.quantity).toLowerCase().includes(searchTermLower) ||
-        transaction.paymentMethod.toLowerCase().includes(searchTermLower) ||
-        formatDate(transaction.date).toLowerCase().includes(searchTermLower))
+      transaction.id.toString().includes(searchTermLower) ||
+      statusMap[transaction.status].toLowerCase().includes(searchTermLower) ||
+      formatCurrency(transaction.quantity).toLowerCase().includes(searchTermLower) ||
+      transaction.paymentMethod.toLowerCase().includes(searchTermLower) ||
+      formatDate(transaction.date).toLowerCase().includes(searchTermLower)
     );
-  });
- 
+  });  
 
   const exportData = props.transactions
     .filter((transaction) => selectedRows.includes(transaction.id.toString()))
@@ -74,75 +69,26 @@ const TransactionTable = (props) => {
 
   const csvData = exportData.length > 0 ? exportData : [{ 'No hay transacciones seleccionadas': '' }];
 
-  const filterTableByStatus = (status) => {
-    setSelectedStatus(status);
-  };
-
-  const handlePageChange = (newPage) => {
-    setCurrentPage(newPage);
-  };
-
-  const totalTransactions = filteredTransactions.length;
-  const totalPages = Math.ceil(totalTransactions / transactionsPerPage);
-
-  const start = (currentPage - 1) * transactionsPerPage;
-  const end = start + transactionsPerPage;
-
-  const displayedTransactions = filteredTransactions.slice(start, end);
-
   return (
     <div>
-        {selectedRows.length > 0 ? (
+      <div className="d-flex justify-content-between mb-3">
+        <input
+          type="text"
+          className="form-control mt-3"
+          placeholder="Buscar..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        {selectedRows.length > 0 && (
           <CSVLink
             data={csvData}
             filename="transactions.csv"
-            className="btn btn-success float-right"
+            className="btn btn-success"
             onClick={clearSelectedRows}
-            
           >
             Exportar seleccionados
           </CSVLink>
-        ) : (
-          <button className="btn btn-success" disabled>Exportar seleccionados</button>
         )}
-      <div className="d-flex justify-content-between mb-3">
-
-          <div className="dropdown mx-2">
-            <button
-              className="btn btn-light dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton1"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Estado
-            </button>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-              {Object.keys(statusMap).map((statusKey) => (
-                <li key={statusKey}>
-                  <a
-                    className="dropdown-item"
-                    href="#"
-                    onClick={() => filterTableByStatus(parseInt(statusKey))}
-                  >
-                    {statusMap[statusKey]}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Buscar..."
-            style={{ width: '50%' }}
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-        
-        
       </div>
 
       <table className="table table-hover mt-3">
@@ -156,8 +102,8 @@ const TransactionTable = (props) => {
           </tr>
         </thead>
         <tbody>
-        {displayedTransactions.length > 0 ? (
-            displayedTransactions.map((transaction) => (
+          {filteredTransactions.length > 0 ? (
+            filteredTransactions.map((transaction) => (
               <tr className='tr-transactions' key={transaction.id.toString()}>
                 <td>
                 <input
@@ -185,25 +131,6 @@ const TransactionTable = (props) => {
           )}
         </tbody>
       </table>
-      <div className="d-flex justify-content-between mt-3">
-        <div>
-          <button
-            className="btn btn-light"
-            onClick={() => handlePageChange(currentPage - 1)}
-            disabled={currentPage === 1}
-          >
-            Anterior
-          </button>
-          <span className="mx-3">Página {currentPage}</span>
-          <button
-            className="btn btn-light"
-            onClick={() => handlePageChange(currentPage + 1)}
-            disabled={currentPage === totalPages}
-          >
-            Siguiente
-          </button>
-        </div>
-      </div>
     </div>
   );
 };
