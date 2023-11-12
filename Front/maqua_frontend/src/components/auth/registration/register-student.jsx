14f48f039f2caaba36dbc7418be002327825c899
ValenAH <valenah52@gmail.com>
2023-11-12 11:28:56 -0500

    removing unuser import and ignore sonar documents

--- Front/maqua_frontend/src/components/auth/registration/register-student.jsx
+++ Front/maqua_frontend/src/components/auth/registration/register-student.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./register-student.scss";
 import { Modal } from "react-bootstrap";
 import logo from "../../../assets/images/logo-maqua.svg";
